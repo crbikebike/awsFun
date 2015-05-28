3382f0eba52f9a4123b5193268c8fcc9a539de64
crbikebike <chris@eatingrd.com>
2015-05-27 17:36:50 -0700

    updated for py3.4

--- sqsapp.py
+++ sqsapp.py
@@ -16,28 +16,39 @@ sqsConn = boto.sqs.connect_to_region("us-west-2",
                                      aws_access_key_id = keyinfo.akey(),
                                      aws_secret_access_key = keyinfo.skey())
 appBucket = s3conn.get_bucket('crace-appstore')
-filePut = file(putMe,mode='rb')
-filePut2 = file(putme2,mode='rb')
-filePut3 = file(putme3,mode='rb')
+filePut = open(putMe,mode='r')
+filePut2 = open(putme2,mode='r')
+filePut3 = open(putme3,mode='r')
 k = Key(appBucket)
 k2 = Key(appBucket)
 
+def countqueue(queueRS):
+    i = 0
+    for message in queueRS:
+        i += 1
+    return i
+
+def printbodies(queueRS):
+    for message in queueRS:
+        print (message.get_body())
+
 if __name__ == '__main__':
-    k2.key = putme2
-    k2.set_contents_from_file(filePut2)
-    k.key = putMe
-    k.set_contents_from_file(filePut)
-    k.key = putme3
-    k.set_contents_from_file(filePut3)
+    # k2.key = putme2
+    # k2.set_contents_from_file(filePut2)
+    # k.key = putMe
+    # k.set_contents_from_file(filePut)
+    # k.key = putme3
+    # k.set_contents_from_file(filePut3)
     my_q = sqsConn.get_queue('cracequeque')
 
-    listBucket = appBucket.list()
-    print 'listing bucket items'
-    for item in listBucket:
-        m = Message()
-        m.set_body(str(item))
-        my_q.write(m)
+    # listBucket = appBucket.list()
+    # print 'listing bucket items'
+    # for item in listBucket:
+    #     m = Message()
+    #     m.set_body(str(item))
+    #     my_q.write(m)
 
-    print 'listing queue'
-    sqsResultSet = my_q.read(60)
-    print sqsResultSet.get_body()
+    print ('listing queue')
+    sqsResultSet = my_q.get_messages()
+    print ('queue is this big: ' + str(countqueue(sqsResultSet)))
+    printbodies(sqsResultSet)
